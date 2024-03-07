98bf14728cacad5eb475882697a044c8c53d1a3e
Bărboi Sabin <113983452+sabin1603@users.noreply.github.com>
2024-03-07 17:15:32 +0200

    Add files via upload

---
+++ Advanced Programming Methods/a2-sabin1603/domain/Car.java
@@ -0,0 +1,52 @@
+package domain;
+
+import java.util.UUID;
+
+public class Car implements Identifiable<String> {
+    private String id;
+    private String nameOfCar;
+    private int fabricationYear;
+    private int km;
+
+    public Car(String id, String nameOfCar, int fabricationYear, int km) {
+        this.id = id;
+        this.nameOfCar = nameOfCar;
+        this.fabricationYear = fabricationYear;
+        this.km = km;
+    }
+
+    public String getNameOfCar() {
+        return nameOfCar;
+    }
+
+    public int getFabricationYear() {
+        return fabricationYear;
+    }
+
+    public int getKm() {
+        return km;
+    }
+
+    public void setNameOfCar(String newNameOfCar) {
+        this.nameOfCar = newNameOfCar;
+    }
+
+    public void setFabricationYear(int newFabricationYear) {
+        this.fabricationYear = newFabricationYear;
+    }
+
+    public void setKm(int newKm) {
+        this.km = newKm;
+    }
+
+
+    public String toString() {
+        return "Car: " + "ID: " + id + "-" + nameOfCar + " - " + fabricationYear + ", car has " + km + " km so far.";
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+}
